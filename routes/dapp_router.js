a92b29fcc260ed3b150ecbf8989c40be22742218
BALAVIGNESHDOSTRIX <crystelpheonix2gmail.com>
2018-07-18 18:07:04 +0530

    public testnet example

--- routes/dapp_router.js
+++ routes/dapp_router.js
@@ -12,9 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 initaPrivate = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
 initaPublic = 'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV'
 keyProvider = initaPrivate
-app.disable('etag');
 
-/*
+
 config = {
   chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7
 
